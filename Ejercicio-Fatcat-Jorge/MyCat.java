e0fd2800516e2d06dd071b3a792634059faa2888
JorgeAndrino21616 <and21616@uvg.edu.gt>
2021-07-20 07:58:46 -0600

    Ejercicio FatCat

---
+++ Ejercicio-Fatcat-Jorge/MyCat.java
@@ -0,0 +1,40 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * MyCat is your own cat. Get it to do things by writing code in its act method.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyCat extends Cat
+{
+    /**
+     * Act - do whatever the MyCat wants to do.
+     */
+    public void act()
+    {
+    if (isHungry())
+        {
+        eat();
+    }
+    if (isBored())
+        {
+        dance();
+    }
+    if (isSleepy())
+    {
+        sleep(5);
+        shoutHooray();
+    }
+    if (isAlone())
+    {
+     sleep(4);
+    }
+    if (hasCompany())
+    {
+        shoutHooray();
+    }
+    }
+    }
+
+
